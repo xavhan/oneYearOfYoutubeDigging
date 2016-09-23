056b3967a1db8ff852f80e7c432ba3a53232bd4b
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2016-09-23 19:56:05 +0200

    use localStorage for faster loading ;)

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import 'spectre.css/dist/spectre.min.css';
 import './App.css';
 import data from './services/data';
-
+import { version } from './../package.json';
 
 class App extends Component {
-  state = { tracks: [] };
+  state = { tracks: JSON.parse(localStorage.getItem(`1yoyd_tracks`)) || [] };
+
   componentDidMount() {
-    data.getTracks().then(tracks => { this.setState({ tracks }); });
+    if (localStorage.getItem(`1yoyd_version`) !== version) {
+      data.getTracks()
+        .then(tracks => {
+          this.setState({ tracks });
+          localStorage.setItem('1yoyd_tracks', JSON.stringify(tracks));
+          localStorage.setItem(`1yoyd_version`, version)
+        })
+    }
   }
+
   render() {
     return (
       <div className="App container">
         <h1>One year of youtube digging</h1>
-        <p>During all the year I did record one track a day forr this project, here is the result :</p>
+        <p>During all the year I did record one track a day for this project, here is the result :</p>
         <ul>
           { this.state.tracks.map((track, i) => (
             <li key={ i }>
