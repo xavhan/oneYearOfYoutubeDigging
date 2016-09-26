2e9e4ed1cd24786c806c7d52121b9a315092d49f
Xavier Haniquaut <xavier.haniquaut@gmail.com>
2016-09-26 11:06:38 +0200

    try to enable coverage

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Storage Mock
+  function storageMock() {
+    var storage = {};
+
+    return {
+      setItem: function(key, value) {
+        storage[key] = value || '';
+      },
+      getItem: function(key) {
+        return storage[key] || null;
+      },
+      removeItem: function(key) {
+        delete storage[key];
+      },
+      get length() {
+        return Object.keys(storage).length;
+      },
+      key: function(i) {
+        var keys = Object.keys(storage);
+        return keys[i] || null;
+      }
+    };
+  }
+  // mock the localStorage
+  window.localStorage = storageMock();
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
